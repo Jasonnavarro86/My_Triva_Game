90635a0c5aaa16baec509956a51392dc6af21af1
Jason Navarro <Jasonnavarro86@yahoo.com>
2017-08-29 00:11:28 -0400

    working on the object and creating divs on the page with jQuery

--- assets/javascript/java.js
+++ assets/javascript/java.js
@@ -1,84 +1,82 @@
-function page(question, answer1, answer2, answer3, answer4) {
-    this.question = question;
-    this.answer1 = answer1;
-    this.answer2 = answer2;
-    this.answer3 = answer3;
-    this.answer4 = answer4;
-    
+var triva = {
+    questions: {
+        q1: {
+            question: "Who Played Spiderman in Spiderman Home Coming?",
+            wrongAns: ["Andrew Garfield?", "Toby Maguire?", "Drake Bell?"],
+            rightAns: "Tom Holland?",
+            gif: "https://68.media.tumblr.com/d59d4e1b5a47de66333625a3fbfa789e/tumblr_oe1fy9kK3N1vyac05o1_500.gif",
+        },
+        q2: {
+            question: "Which hero is from the planet Asgard?",
+            wrongAns: ["Black Panther?", "Hulk?", "Antman?"],
+            rightAns: "Thor",
+            gif: "https://media.giphy.com/media/Ch1zCx8tu6DQY/giphy.gif"
+        },
+        q3: {
+            question: "Who's is in charge of the Avengers?",
+            wrongAns: ["Captain America?", "Iron Man?", "Barry Allen?"],
+            rightAns: "Nick Fury",
+            gif: "https://media.giphy.com/media/3GKWesp8SVVGo/giphy.gif"
+        },
+        q4: {
+            question: "What is Captain America's real name?",
+            wrongAns: ["Tony Stark?", "Stephen Myers?", "Peter Parker?"],
+            rightAns: "Steve Rodgers?",
+            gif: "https://media.giphy.com/media/1lk1IcVgqPLkA/giphy.gif"
+        },
+        q5: {
+            question: "Who is the founder of Marvel?",
+            wrongAns: ["Walt Disney?", "Hana Barbera?", "Mark Hamill?"],
+            rightAns: "Stan Lee?",
+            gif: "https://orig03.deviantart.net/e0e5/f/2013/189/8/b/the_amazing_stan_lee_gif_animated__by_thejigsawrlm-d6cj9l0.gif"
+        },
+        q6: {
+            question: "What is The Flash's real name?",
+            wrongAns: ["Ben Walker?", "Mark Hamill?", "Norrin Rad?"],
+            rightAns: "Berry Allen?",
+            gif: "http://data.whicdn.com/images/53818085/original.gif"
+        }
+
+    },
+
+    theQuestion: "question",
+    theWrong: "wrongAns",
+    theRight: "rightAns",
+    theGif: "gif",
+    theGifShown: "",
+
+    startIt: function (qq) {
+
+        var count = 0;
+        this.theGifShown = qq[this.theGif]
+        this.answerArray = qq[this.theWrong];
+
+        $("<div>").addClass("container theQuestion buttons").html(qq[this.theQuestion])
+            .appendTo('body');
+
+        for (var i = 0; i < this.answerArray.length; i++) {
+            count++;
+
+            $("<div>").addClass("container")
+                .html(this.answerArray[i])
+                .appendTo('body');
+        }
+
+        $("<div>").addClass("container buttons")
+            .html(qq[this.theRight])
+            .appendTo(".answer" + count);
+
+
+
+
+
+
+       
+        //     // this.displayThem(qq);
+
+    },
+
+
 }
 
-var page1 = new page("are you old?", "yes", "no", "blue", "ques");
-var page2 = new page("Sally", "Rally", 48, "green");
-var page3 = new page("Kim", "green", 48, "june")
-var page4 = new page("are you old?", "yes", "no", "blue", "ques");
-var page5 = new page("Sally", "Rally", 48, "green");
-var page6 = new page("Kim", "green", 48, "june")
-
-function check(px) {
-    var counter = 0;
-
-    $.each(px, function (key, value) {
-        counter++;
-
-        if (key === "question") {
-            $(".question").append("<h1>" + value + "</h1>")
-           
-        } else {
-            
-        $(".answer").append("<h2 id='line" +counter+ "'>"+value+"</h2>")
-        
-        }
-        if(px === page1){
-            $("#line2").insertAfter("#line4");
-        }
-        if(px === page2){
-            $("#line2").insertAfter("#line2");
-        }
-        if(px === page3){
-            $("#line2").insertAfter("#line5");
-        }
-        if(px === page4){
-            $("#line2").insertAfter("#line3");
-        }
-        if(px === page5){
-            $("#line2").insertAfter("#line4");
-        }
-        if(px === page6){
-            $("#line2").insertAfter("#line3");
-        }
-    })
-    myScore(px);
-}
-   
-function myScore(){
-$("h2").on("click", function(px){
-    
-                if($(this) === px.answer1){
-                   console.log("if");
-                }
-    
-                else{
-                    console.log("else");
-                }
-            })
-        }
-    
-    
-
-
-
-
-
-
-function animate(){
-
-}
-
-
-
-
-
-check(page1);
-// check(page2);
-// check(page3);
-// check(page4);
+triva.startIt(triva.questions.q1);
