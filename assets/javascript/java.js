152608a24a44493dec602fcefd87d4bbd942872b
Jason Navarro <Jasonnavarro86@yahoo.com>
2017-08-30 14:24:38 -0400

    added a start button that I over looked. about 97% done with it all

--- assets/javascript/java.js
+++ assets/javascript/java.js
@@ -1,13 +1,14 @@
 // START: My triva info inside of an object that is inside an array. 
 var questions = [{
-        question: "Who Played Spiderman in Spiderman Home Coming?",
+        question: "Who Played Spiderman in Spiderman-Home-Coming?",
         Ans1: {
             q1: "Andrew Garfield",
             q2: "Toby Maguire",
             q4: "Drake Bell",
             q3: "Tom Holland",
         },
-        gif: "<img src='https://68.media.tumblr.com/d59d4e1b5a47de66333625a3fbfa789e/tumblr_oe1fy9kK3N1vyac05o1_500.gif'/>",
+        gif: "<img src='https://static.tumblr.com/cb658e41b278cf7c509dd817120230e5/6i1maex/sCrotlima/tumblr_static_tumblr_static_filename_640.gif'/>",
+        gif2: "<img src='https://68.media.tumblr.com/bf6e2da24704119891b88522a7a96ed0/tumblr_inline_ou9zoxE4uy1t9q8c4_540.gif'/>"
     },
     {
         question: "Which hero is from the planet Asgard?",
@@ -17,7 +18,8 @@ var questions = [{
             q2: "Hulk",
             q4: "Antman"
         },
-        gif: "<img src='https://media.giphy.com/media/Ch1zCx8tu6DQY/giphy.gif'/>"
+        gif: "<img src='https://media.giphy.com/media/Ch1zCx8tu6DQY/giphy.gif'/>",
+        gif2: "<img src='http://www.lovethisgif.com/uploaded_images/63193-Guardians-Of-The-Galaxy-Rocket-Gif-Reaction-Gif.gif'/>"
     },
     {
         question: "Who's is in charge of the Avengers?",
@@ -27,8 +29,10 @@ var questions = [{
             q4: "Barry Allen",
             q3: "Nick Fury"
         },
-        gif: "<img src='https://media.giphy.com/media/3GKWesp8SVVGo/giphy.gif'/>"
+        gif: "<img src='https://media.giphy.com/media/3GKWesp8SVVGo/giphy.gif'/>",
+        gif2: "<img src='http://68.media.tumblr.com/tumblr_mbfx76wyxn1r0c27po1_250.gif'/>"
     },
+
     {
         question: "What is Captain America's real name?",
         Ans1: {
@@ -37,7 +41,8 @@ var questions = [{
             q2: "Stephen Myers",
             q4: "Peter Parker"
         },
-        gif: "<img src='https://media.giphy.com/media/1lk1IcVgqPLkA/giphy.gif'/>"
+        gif: "<img src='https://media1.popsugar-assets.com/files/thumbor/4E_ni6DV9ne4oFG97W_cP_ta8pI/fit-in/1024x1024/filters:format_auto-!!-:strip_icc-!!-/2015/04/21/871/n/1922283/4d253a24efb7305a_3673790-2595346539-capta/i/He-Knows.gif'/>",
+        gif2: "<img src='https://media.giphy.com/media/o80k0RHRR4qQM/giphy.gif'/>"
     },
     {
         question: "Who is the founder of Marvel?",
@@ -47,7 +52,8 @@ var questions = [{
             q2: "Mark Hamill",
             q4: "Hana Barbera"
         },
-        gif: "<img src='https://orig03.deviantart.net/e0e5/f/2013/189/8/b/the_amazing_stan_lee_gif_animated__by_thejigsawrlm-d6cj9l0.gif'/>"
+        gif: "<img src='https://orig03.deviantart.net/e0e5/f/2013/189/8/b/the_amazing_stan_lee_gif_animated__by_thejigsawrlm-d6cj9l0.gif'/>",
+        gif2: "<img src='https://typeset-beta.imgix.net/2017/4/20/cbe35063-53d4-4939-b337-6a54852c4b4a.gif'/>"
     },
     {
         question: "What is The Flash's real name?",
@@ -70,6 +76,7 @@ var timer1;
 // START: This function () Creates all my html with jQuery holding the object info. 
 function createPage() {
     // These vars take my Ans1 object properties and stores them in an array (var storeQs) that allows me to access the answers.
+    $(".start").hide()
     var storeQs = Object.keys(questions[pageNum].Ans1)
     var divQ1 = storeQs[0];
     var divQ2 = storeQs[1];
@@ -82,7 +89,6 @@ function createPage() {
     $("<div>").addClass("container answer btn").html(questions[pageNum].Ans1[divQ3] + "?").appendTo("body")
     $("<div>").addClass("container answer btn").html(questions[pageNum].Ans1[divQ4] + "?").appendTo("body")
 }
-createPage();
 // START: This function() is for when a player clicks an answer the next functions are called. 
 function getAnswer() {
     $(".answer").on("click", function () {
@@ -97,7 +103,6 @@ function getAnswer() {
         }
     })
 }
-getAnswer();
 // START: If the player clicks the correct answer this function() clears timers and hides divs created before and creates new ones with "great job!" info, and calls timer. 
 function animateRight() {
     clearTimeout(timer);
@@ -115,7 +120,7 @@ function animateWrong() {
     $(".question").hide()
     $(".answer").hide()
     $("<div>").addClass("animateBox answer1 container").html("Ouch! " + questions[pageNum].Ans1.q3 + " was the right answer.").appendTo("body")
-    $("<div>").addClass("gifBox text-ceter container").html(questions[5].gif2).appendTo("body")
+    $("<div>").addClass("gifBox text-ceter container").html(questions[pageNum].gif2).appendTo("body")
     goodTimer(4, "#timer");
 }
 // START: This function () resets the new page after the two animate right/wrong functions() appear and calls the main page timer.
@@ -177,11 +182,21 @@ function scoreDisplay() {
 function startTimer() {
     timedOut(20, "#timer");
 }
-startTimer();
+
 // START: 
 function fixScoreDisplay() {
     if (pageNum == 0) {
-    $(".question").hide()
-    $(".answer").hide()
+        $(".question").hide()
+        $(".answer").hide()
     }
 }
+
+function startButton() {
+    $("<button>").addClass("start btn btn lg btn-danger").html("AVENGERS TRIVA GAME! <br> " + " click to start.").appendTo("body")
+    $(".start").on("click", function () {
+        createPage();
+        getAnswer();
+        startTimer();
+    })
+}
+startButton();
